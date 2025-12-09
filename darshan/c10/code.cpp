c0eca76bae9e5177f13ed23bf3a5c8d4cfa127b3
darshanbcs138 <01fe24bcs138@kletech.ac.in>
2025-12-09 21:42:40 +0530

    Update code.cpp

--- darshan/c10/code.cpp
+++ darshan/c10/code.cpp
@@ -1,1 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int parent[100], rnk[100];
+
+int findp(int x){
+    if(parent[x]==x) return x;
+    return parent[x]=findp(parent[x]);
+}
+
+bool unite(int a,int b){
+    a=findp(a);
+    b=findp(b);
+    if(a==b) return false;
+    if(rnk[a]<rnk[b]) swap(a,b);
+    parent[b]=a;
+    if(rnk[a]==rnk[b]) rnk[a]++;
+    return true;
+}
+
+int main(){
+    int n,m;
+    cin>>n>>m;
+    for(int i
